f980fe320957bcf9e2e984f7d63775fca277170a
whcsdtc <whcwuhanchu@gmail.com>
2017-07-25 17:30:47 +0800

    去除无用模块evidence

--- app/modules/evidences/parameters.py
+++
@@ -1,6 +0,0 @@
-# encoding: utf-8
-"""
-Input arguments (Parameters) for Team resources RESTful API
------------------------------------------------------------
-"""
-
--- app/modules/evidences/schemas.py
+++
@@ -1,6 +0,0 @@
-# encoding: utf-8
-"""
-Serialization schemas for Team resources RESTful API
-----------------------------------------------------
-"""
-
